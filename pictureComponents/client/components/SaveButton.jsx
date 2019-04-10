afe52a963dedd9a641d668bc04ba22db6e8b807d
Sutton <suco3693@gmail.com>
2019-04-09 19:52:13 -0700

    Update to css and addition of apartment amenities svg tags

--- pictureComponents/client/components/SaveButton.jsx
+++ pictureComponents/client/components/SaveButton.jsx
@@ -5,7 +5,7 @@ import '../style.module.css';
 const SaveButton = (props) => {
   if (props.windowWidth > 745) {
     return (
-      <button type="button" className="saveButton button">
+      <button type="button" className="saveButton photobutton">
         <span>
           <img src="https://s3-us-west-1.amazonaws.com/staycloudphotobucket/svg/heart.svg" className="buttonIcon" />
         </span>
--- pictureComponents/client/components/ShareButton.jsx
+++ pictureComponents/client/components/ShareButton.jsx
@@ -5,7 +5,7 @@ import '../style.module.css';
 const ShareButton = (props) => {
   if (props.windowWidth > 745) {
     return (
-      <button type="button" className="shareButton button">
+      <button type="button" className="shareButton photobutton">
         <span>
           <img src="https://s3-us-west-1.amazonaws.com/staycloudphotobucket/svg/share.svg" className="buttonIcon sharePic" />
         </span>
