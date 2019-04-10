617bf2014b34ab7023179e70c23be9a916b80a69
Sutton <suco3693@gmail.com>
2019-04-10 16:01:14 -0700

    Trimming of files for hosting on aws

--- infoComponent/client/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-import "./style.module.css"
-
-ReactDOM.render(<App/>, document.getElementById('info'));
