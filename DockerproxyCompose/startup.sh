3e5fcd3c682390c6239de554de0e267f27c967b2
Sutton <suco3693@gmail.com>
2019-04-17 19:28:06 -0700

    Addition of startup script to use on ec2 for start up

---
+++ DockerproxyCompose/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker start ec2-user_listingdatabase_1
+docker start ec2-user_reviewdatabase_1
+docker start ec2-user_infodatabase_1
+docker start ec2-user_cloudstaypicdb_1
+docker start ec2-user_bookingdatabase_1
+docker start ec2-usr_proxyserver_1
+docker start ec2-user_bookings_1
+docker start ec2-user_piccont_1
+docker start ec2-user_info_1
+docker start ec2-user_review_1
+docker start ec2-user_listings_1
